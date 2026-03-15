8c705e9f52207f5aaaa76d70c76769c4d21749e4
iamvince24 <yohongguo24@gmail.com>
2026-03-15 22:44:30 +0800

    feat(config): enhance Vite configuration for production mode
    - Updated vite.config.ts to include conditional esbuild settings for production mode, allowing for the removal of console logs and debug information in the final build.

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,17 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+  esbuild: {
+    pure:
+      mode === "production"
+        ? ["console.log", "console.info", "console.debug"]
+        : [],
+  },
+}));
